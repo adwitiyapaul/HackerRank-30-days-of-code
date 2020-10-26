8121a838d2804da087d5ab8e3583730e9aac9989
Adwitiya Paul <adwitiyapaul8@gmail.com>
2020-10-26 22:24:48 +0530

    Solution.py

---
+++ 0_Hello_World/Solution.py
@@ -0,0 +1,16 @@
+#Task
+#To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World. on a single line, 
+#and finally print the value of your variable on a second line.
+
+#Input Format
+#A single line of text denoting inputString (the variable whose contents must be printed).
+
+#Output Format
+#Print Hello, World. on the first line, and the contents of inputString on the second line.
+
+#Solution
+
+input_string = input()
+
+print('Hello, World.')
+print(input_string)
